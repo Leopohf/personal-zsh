22db9ba8f916c4695a08372c9fadf0ad2b16a479
Jorge L Hernandez F <leopohf@gmail.com>
2026-07-03 08:22:52 -0500

    enable git lfs and add plugin

---
+++ config/plugins/git-lfs.zsh
@@ -0,0 +1,12 @@
+# Git LFS Configuration
+# Oh My Zsh Git LFS plugin aliases wrapper
+alias glfs='git lfs'
+alias glfsi='git lfs install'
+alias glfsu='git lfs uninstall'
+alias glfst='git lfs track'
+alias glfsut='git lfs untrack'
+alias glfsl='git lfs ls-files'
+alias glfsp='git lfs push'
+alias glfspf='git lfs push --force'
+alias glfspn='git lfs push --dry-run'
+alias glfssa='git lfs status'
